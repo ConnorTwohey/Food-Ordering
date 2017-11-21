9d9de5d692aaf47c984c816c836b37eca1f4ee85
minhtea <minhtree124@gmail.com>
2017-11-21 17:15:24 -0600

    it works

--- form-dessert.php
+++ form-dessert.php
@@ -6,70 +6,11 @@
 </style>
 </head>
 <body>
-<?php
-		include 'insert-dessert.php';
-		
-		// define variables and set to empty values
-		$pnameErr = $descripErr = $priceErr = $imageErr = $stockErr = $calErr = $fatsErr = "";
-		$pname = $descrip = $image = "";
-		$price = $stock = $cal = $fats = $iscold = 0;
-		
-		if ($_SERVER["REQUEST_METHOD"] == "POST"){
-			  $pname = test_input($_POST["PName"]);
-			  $descrip = test_input($_POST["Description"]);
-			  $price = $_POST["Price"];
-			  $stock = $_POST["Stock"];
-			  $cals = $_POST["Calories"];
-			  $fats = $_POST["Fats"];
-			  $iscold = $_POST["Is_Cold"];
-			  
-			  echo "$pname, $descrip, $price, $image, $stock, $cal, $fats, $iscold";
-			  
-			  $file = $_POST["Image"];
-			  $image = fopen("$file", "rb");
-			  
-			  if (empty($pname)) {
-				$pnameErr = "Name is required";
-			  }
-			  
-			  if (empty($descrip)) {
-				  $descripErr = "Description is required";
-			  }
-			  
-			  if (empty($price) || $price <= 0) {
-				$priceErr = "Price has to be above 0";
-			  }
-			  
-			  if (empty($image)) {
-				$imageErr = "Image is required";
-			  }
-			  
-			  if ($stock < 0) {
-				$stockErr = "Number in stock is required";
-			  }
-			  
-			  if ($cals < 0) {
-				$phoneErr = "Calories are required";
-			  }
-			  
-			  if ($fats < 0) {
-				$fatsErr = "Email is required";
-			  }
-			  
-			  if($nameErr == "" && $passwordErr == "" && $finitErr == "" && $lnameErr == "" && $addressErr == "" && $phoneErr == "" && $emailErr == ""){
-					echo "Form has been sent.<br>";
-				  	echo "$pname, $descrip, $price, $fpath, $stock, $cal, $fats, $iscold";
-					echo "Query has been sent<br>";
-					
-			  }
-		}
-	?>
-	
 	<h2>Dessert Input Form</h2>
     
     <p><span class="error">* required field.</span></p>   
     
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+	<form action="insert-dessert.php" method="post" enctype="multipart/form-data">
 	Product name: <input type="text" name="PName" value="<?php echo $pname;?>" maxlength="32">
     	<span class="error">* <?php echo $pnameErr;?></span><br><br>
         
@@ -95,6 +36,8 @@
 	<input type="submit" name="submit" value="Submit">  
 	</form>
 	<p>Click the "Submit" button to input Dessert data.</p>
+    
+    
 	<p><a href="http://localhost:8888/">&lt;Back to index&gt;</a></p>
     
 </body>
--- insert-dessert.php
+++ insert-dessert.php
@@ -4,55 +4,72 @@
 	define('DB_PASSWORD', 'root');
 	define('DB_NAME', 'Online_Food_Ordering');
 	
- 	function insert_dessert($pname, $descrip, $price, $fpath, $stock, $cal, $fats, $iscold){
-		try{
-			$pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+ 	try{
+		$pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+	
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			
-			print "Connected to database. <br><br>";
-		
-		} catch(PDOException $error){
-			die("ERROR: Could not connect. " . $error->getMessage());
-		}
-		
-		$pdo->beginTransaction();
-		print "Transaction has begun.<br>";
-		
-		print "Locking.<br>";
-		$pdo->exec('LOCK TABLES `Product` WRITE');
-		print "Customer table is locked<br>";
-		
-		
-		try{
-			$stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, : stock, :calories, :fats);");
-			
-			$stmt->bindParam(':name', $name, PDO::PARAM_STR, 12);
-			$stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
-			$stmt->bindParam(':price', $price);
-			$stmt->bindParam(':image', $image);
-			$stmt->bindParam(':stock', $name, PDO::PARAM_INT);
-			$stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
-			$stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
-			$stmt->execute();
-			
-			$pid = $pdo->query("SELECT ProductId WHERE Product_Name=$name");
-			
-			$stmt = $pdo->prepare("INSERT INTO `Dessert` (`Pid`, `IsCold`) VALUES (:pid, :iscold);");
-			$stmt->bindParam(':pid', $pid);
-			$stmt->bindParam(':iscold', $iscold);
-			
-            $pdo->commit();
-	  
-		    $pdo->exec('UNLOCK TABLES');
-		    print "Unlock table. Successful transaction<br>";
-		
-		    $pdo = null;
-		}
-		catch(PDOException $error) {
-			$pdo->rollback();
-			print("Rollingback <br>");
-			die("ERROR: Could not complete. " . $error->getMessage());
-		}
+		print "Connected to database. <br><br>";
+	
+	} catch(PDOException $error){
+		die("ERROR: Could not connect. " . $error->getMessage());
 	}
-	?> 
+	
+	$name = $_POST["PName"];
+	$descrip = $_POST["Description"];
+	$price = $_POST["Price"];
+	$image =file_get_contents($_FILES  ['Image']['tmp_name']);	
+	$stock = $_POST["Stock"];
+	$calories = $_POST["Calories"];
+	$fats = $_POST["Fats"];
+	$iscold = $_POST["Is_Cold"];	  
+	
+	$pdo->beginTransaction();
+	print "Transaction has begun.<br>";
+	
+	print "Locking.<br>";
+	$pdo->exec('LOCK TABLES `Product` WRITE');
+	print "Customer table is locked<br>";
+	
+	
+	try{
+		$stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, :stock, :calories, :fats);");
+		
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
+		$stmt->bindParam(':price', $price);
+		$stmt->bindParam(':image', $image);
+		$stmt->bindParam(':stock', $stock, PDO::PARAM_INT);
+		$stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
+		$stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
+		$stmt->execute();
+		
+		print("Finding ProductID to reference<br>");
+		
+		$stmt = $pdo->perpare("SELECT ProductId WHERE Product_Name = :name");
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		$pid = $row['ProductId'];
+		
+		
+		print("ProductID $pid found.<br>");
+		
+		$stmt = $pdo->prepare("INSERT INTO `Dessert` (`Pid`, `IsCold`) VALUES (:pid, :iscold);");
+		$stmt->bindParam(':pid', $pid);
+		$stmt->bindParam(':iscold', $iscold);
+		$stmt->execute();
+		
+		$pdo->commit();
+  
+		$pdo->exec('UNLOCK TABLES');
+		print "Unlock table. Successful transaction<br>";
+	
+		$pdo = null;
+	}
+	catch(PDOException $error) {
+		$pdo->rollback();
+		print("Rollingback <br>");
+		die("ERROR: Could not complete. " . $error->getMessage());
+	}
+?> 
