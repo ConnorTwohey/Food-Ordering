707f30efd24f64219aca1882b26cdd1329413779
Connor Twohey <connortwohey@Connors-MacBook-Pro.local>
2017-11-21 18:39:22 -0600

    salad finished

--- form-salad.php
+++ form-salad.php
@@ -6,44 +6,39 @@
 </style>
 </head>
 <body>
-<?php
-  include 'insert-salad.php';
+	<?php
+        include 'insert-salad.php';
 
-  $pnameErr = $descripErr = $priceErr = $imageErr = $stockErr = $calErr = $fatsErr = $sizeErr = "";
-		$pname = $descrip = $image = $size = "";
-		$price = $stock = $cal = $fats = $dressing = 0;
+		$pnameErr = $descripErr = $priceErr = $imageErr = $stockErr = $calErr = $fatsErr = $sizeErr = $dressingErr = "";
+        $pname = $descrip = $image = $size = "";
+        $price = $stock = $cal = $fats = $dressing = 0;
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    			  $pname = $_POST["PName"];
-    			  $descrip = $_POST["Description"];
-    			  $price = $_POST["Price"];
-    			  $stock = $_POST["Stock"];
-    			  $cals = $_POST["Calories"];
-    			  $fats = $_POST["Fats"];
-            $size = $_POST["Size"];
-            $dressing = $_POST["Dressing"];
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+			$pname = $_POST["PName"];
+			$descrip = $_POST["Description"];
+			$price = $_POST["Price"];
+			$image =file_get_contents($_FILES  ['Image']['tmp_name']);
+			$stock = $_POST["Stock"];
+			$cal = $_POST["Calories"];
+			$fats = $_POST["Fats"];
+			$size= $_POST["Size"];
+			$dressing = $_POST["Has_Dressing"];
 
-            $file = $_POST["Image"];
-            $image = fopen("$file", "rb");
-
-            echo "$pname, $descrip, $price, $image, $stock, $cal, $fats, $size, $dressing";
-
-
-            if (empty($pname)) {
+			if (empty($pname)) {
 				$pnameErr = "Name is required";
-			  }
+			}
 
-			  if (empty($descrip)) {
+			if (empty($descrip)) {
 				  $descripErr = "Description is required";
-			  }
+			}
 
-			  if (empty($price) || $price <= 0) {
+			if ($price <= 0) {
 				$priceErr = "Price has to be above 0";
-			  }
+			}
 
-			  if (empty($image)) {
+			if (empty($image)) {
 				$imageErr = "Image is required";
-			  }
+			}
 
 			  if ($stock < 0) {
 				$stockErr = "Number in stock is required";
@@ -56,15 +51,13 @@
 			  if ($fats < 0) {
 				$fatsErr = "Fats are required";
 			  }
+			  if (empty($size)) {
+				  $sizeErr = "Size is required";
+			  }
 
-        if(empty($size)) {
-          $sizeErr = "Size is required";
-        }
-
-
-			  if($nameErr == "" && $passwordErr == "" && $finitErr == "" && $lnameErr == "" && $addressErr == "" && $phoneErr == "" && $emailErr == "" && $sizeErr == ""){
+			  if($pnameErr == "" && $descripErr == "" && $priceErr == "" && $imageErr == "" && $stockErr == "" && $calErr == "" && $fatsErr == "" && $sizeErr == "" && $dressingErr == ""){
 					echo "Form has been sent.<br>";
-				  	echo "$pname, $descrip, $price, $fpath, $stock, $cal, $fats, $size";
+					insert_salad($pname, $descrip, $price, $image, $stock, $cal, $fats, $size, $dressing);
 					echo "Query has been sent<br>";
 
 			  }
@@ -75,37 +68,39 @@
 
   <p><span class="error">* required field.</span></p>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-Product name: <input type="text" name="PName" value="<?php echo $pname;?>" maxlength="32">
-    <span class="error">* <?php echo $pnameErr;?></span><br><br>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+	Product name: <input type="text" name="PName" value="<?php echo $pname;?>" maxlength="32">
+    	<span class="error">* <?php echo $pnameErr;?></span><br><br>
 
-  Description: <input type="text" name="Description" value="<?php echo $descrip;?>" rows="3" cols="40" maxlength="256">
-    <span class="error">* <?php echo $descripErr;?></span><br><br>
+  	Description: <input type="text" name="Description" value="<?php echo $descrip;?>" rows="3" cols="40" maxlength="256">
+    	<span class="error">* <?php echo $descripErr;?></span><br><br>
 
-  Price: <input type="number" step="0.01" name="Price" value="<?php echo $price;?>" maxlength="1">
-    <span class="error">* <?php echo $priceErr;?></span><br><br>
+  	Price: <input type="number" step="0.01" name="Price" value="<?php echo $price;?>" maxlength="1">
+    	<span class="error">* <?php echo $priceErr;?></span><br><br>
 
-  Image: <input type="file" name="Image" value="Upload Image">
-    <span class="error">* <?php echo $imageErr;?></span><br><br>
+  	Image: <input type="file" name="Image" value="Upload Image">
+    	<span class="error">* <?php echo $imageErr;?></span><br><br>
 
-Stock: <input type="number" name="Stock" value="<?php echo $address;?>" min="0">
-    <span class="error">* <?php echo $addressErr;?></span><br><br>
+	Stock: <input type="number" name="Stock" value="<?php echo $stock;?>" min="0">
+    	<span class="error">* <?php echo $stockErr;?></span><br><br>
 
-Calories: <input type="number" name="Calories" value="<?php echo $phone;?>" min="0">
-    <span class="error">* <?php echo $phoneErr;?></span><br><br>
+	Calories: <input type="number" name="Calories" value="<?php echo $cals;?>" min="0">
+    	<span class="error">* <?php echo $calErr;?></span><br><br>
 
-  Fats: <input type="number" name="Fats" value="<?php echo $email;?>" min="0">
-    <span class="error">* <?php echo $emailErr;?></span><br><br>
+  	Fats: <input type="number" name="Fats" value="<?php echo $fats;?>" min="0">
+    	<span class="error">* <?php echo $fatsErr;?></span><br><br>
+   	Size: <select name="Size">
+    	<option value="">Select...</option>
+		<option value="S">S</option>
+		<option value="M">M</option>
+		<option value="L">L</option>
+ 	</select><span class="error">* <?php echo $sizeErr;?></span><br><br>
 
-  Size: <input type="text" name="Size" value="<?php echo $size;?>" maxlength="1">
-    <span class="error">* <?php echo $sizeErr;?></span><br><br>
-
-    Has Dressing: <input type="text" name="Dressing" value="<?php echo $dressing;?>" maxlength="1">
-
-
-    <input type="submit" name="submit" value="Submit">
-  </form>
-  <p>Click the "Submit" button to input Salad data.</p>
+    Has Dressing: <input type="number" name="Has_Dressing" value="<?php echo $dressing;?>" min="0" max="1">
+    	&nbsp;&nbsp;&nbsp;&nbsp;1 if dressing, 0 if not. <br><br>
+	<input type="submit" name="submit" value="Submit">
+	</form>
+	<p>Click the "Submit" button to input Salad data.</p>
 <p><a href="http://localhost:8888/">&lt;Back to index&gt;</a></p>
 
 </body>
--- insert-salad.php
+++ insert-salad.php
@@ -4,58 +4,65 @@
   define('DB_PASSWORD', 'root');
   define('DB_NAME', 'Online_Food_Ordering');
 
-  function insert_salad($pname, $descrip, $price, $fpath, $stock, $cal, $fats, $size, $dressing) {
+  function insert_salad($name, $descrip, $price, $image, $stock, $calories, $fats, $size, $dressing) {
     try{
-      $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+		$pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
 
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      print "Connected to database. <br><br>";
+		print "Connected to database. <br><br>";
 
-    } catch(PDOException $error){
-      die("ERROR: Could not connect. " . $error->getMessage());
-    }
+	} catch(PDOException $error){
+		die("ERROR: Could not connect. " . $error->getMessage());
+	}
 
-    $pdo->beginTransaction();
-    print "Transaction has begun.<br>";
+	$pdo->beginTransaction();
+	print "Transaction has begun.<br>";
 
-    print "Locking.<br>";
-    $pdo->exec('LOCK TABLES `Product` WRITE');
-    print "Product table is locked<br>";
+	print "Locking.<br>";
+	$pdo->exec('LOCK TABLES `Product` WRITE, `Salad` WRITE');
+	print "Customer table is locked<br>";
 
-    try {
-      $stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, : stock, :calories, :fats);");
 
-      $stmt->bindParam(':name', $pname, PDO::PARAM_STR, 12);
-      $stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
-      $stmt->bindParam(':price', $price);
-      $stmt->bindParam(':image', $image);
-      $stmt->bindParam(':stock', $stock, PDO::PARAM_INT);
-      $stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
-      $stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
-      $stmt->execute();
+	try{
+		$stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, :stock, :calories, :fats);");
 
-      $stmt = $pdo->prepare("SELECT ProductId FROM Product WHERE Product_Name = :pname");
-      $stmt->bindParam(':pname', $pname, PDO::PARAM_STR);
-      $stmt->execute();
-      $row = $stmt->fetch();
-      $pid = $row['ProductId'];
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
+		$stmt->bindParam(':price', $price);
+		$stmt->bindParam(':image', $image);
+		$stmt->bindParam(':stock', $stock, PDO::PARAM_INT);
+		$stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
+		$stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
+		$stmt->execute();
 
-      $stmt = $pdo->prepare("INSERT INTO `Salad` (`Pid`, `HasDressing`) VALUES (:pid, :dressing);");
-      $stmt->bindParam(':pid',$pid);
-      $stmt->bindParam(':dressing',$dressing);
+		print("Finding ProductID to reference<br>");
 
-      $pdo->commit();
+		$stmt = $pdo->prepare("SELECT ProductId FROM Product WHERE Product_Name = :name;");
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		$pid = $row['ProductId'];
 
-      $pdo->exec('UNLOCK TABLES');
+		print("ProductID $pid found.<br>");
 
-      $pdo = null;
+		$stmt = $pdo->prepare("INSERT INTO `Salad` (`Pid`, `Size`, `HasDressing`) VALUES (:pid, :size, :dressing);");
+		$stmt->bindParam(':pid', $pid, PDO::PARAM_INT);
+		$stmt->bindParam(':size', $size);
+		$stmt->bindParam(':dressing', $dressing, PDO::PARAM_INT);
+		$stmt->execute();
+		
+		$pdo->commit();
 
-    }
-    catch(PDOException $error) {
-			$pdo->rollback();
-			print("Rollingback <br>");
-			die("ERROR: Could not complete. " . $error->getMessage());
-		}
+		$pdo->exec('UNLOCK TABLES');
+		print "Unlock table. Successful transaction<br>";
+
+		$pdo = null;
+	}
+	catch(PDOException $error) {
+		$pdo->rollback();
+		print("Rollingback <br>");
+		die("ERROR: Could not complete. " . $error->getMessage());
+	}
   }
 ?>
