6a59d4a12847d0058aad77573d5312b47eaf7cff
minhtea <minhtree124@gmail.com>
2017-11-21 18:06:47 -0600

    insert maindish

--- insert-maindish.php
+++ insert-maindish.php
@@ -4,59 +4,65 @@
   define('DB_PASSWORD', 'root');
   define('DB_NAME', 'Online_Food_Ordering');
 
-  function insert_maindish($pname, $descrip, $price, $fpath, $stock, $cal, $fats, $size, $isHot) {
+  function insert_maindish($name, $descrip, $price, $image, $stock, $calories, $fats, $size, $ishot) {
     try{
-      $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      print "Connected to database. <br><br>";
-
-    } catch(PDOException $error){
-      die("ERROR: Could not connect. " . $error->getMessage());
-    }
-
-    $pdo->beginTransaction();
-    print "Transaction has begun.<br>";
-
-    print "Locking.<br>";
-    $pdo->exec('LOCK TABLES `Product` WRITE');
-    print "Product table is locked<br>";
-
-    try {
-      $stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, : stock, :calories, :fats);");
-
-      $stmt->bindParam(':name', $pname, PDO::PARAM_STR, 12);
-      $stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
-      $stmt->bindParam(':price', $price);
-      $stmt->bindParam(':image', $image);
-      $stmt->bindParam(':stock', $stock, PDO::PARAM_INT);
-      $stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
-      $stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
-      $stmt->execute();
-
-      $stmt = $pdo->prepare("SELECT ProductId FROM Product WHERE Product_Name = :pname");
-      $stmt->bindParam(':pname', $pname, PDO::PARAM_STR);
-      $stmt->execute();
-      $row = $stmt->fetch();
-      $pid = $row['ProductId'];
-
-      $stmt = $pdo->prepare("INSERT INTO `MainDish` (`Pid`, `Size`, `IsHot`) VALUES (:pid, :size, :hot);");
-      $stmt->bindParam(':pid',$pid);
-      $stmt->bindParam(':size',$size);
-      $stmt->bindParam(':hot',$isHot);
-
-      $pdo->commit();
-
-      $pdo->exec('UNLOCK TABLES');
-
-      $pdo = null;
-
-    }
-    catch(PDOException $error) {
-			$pdo->rollback();
-			print("Rollingback <br>");
-			die("ERROR: Could not complete. " . $error->getMessage());
-		}
+		$pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+	
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		print "Connected to database. <br><br>";
+	
+	} catch(PDOException $error){
+		die("ERROR: Could not connect. " . $error->getMessage());
+	}	  
+	
+	$pdo->beginTransaction();
+	print "Transaction has begun.<br>";
+	
+	print "Locking.<br>";
+	$pdo->exec('LOCK TABLES `Product` WRITE, `MainDish` WRITE');
+	print "Customer table is locked<br>";
+	
+	
+	try{
+		$stmt = $pdo->prepare("INSERT INTO `Product` (`Product_Name`, `Description`, `Price`, `Product_Image`, `Num_In_Stock`, `Calories`, `Fats`)  VALUES (:name, :desc, :price, :image, :stock, :calories, :fats);");
+		
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->bindParam(':desc', $descrip, PDO::PARAM_STR, 256);
+		$stmt->bindParam(':price', $price);
+		$stmt->bindParam(':image', $image);
+		$stmt->bindParam(':stock', $stock, PDO::PARAM_INT);
+		$stmt->bindParam(':calories', $calories, PDO::PARAM_INT);
+		$stmt->bindParam(':fats', $fats, PDO::PARAM_INT);
+		$stmt->execute();
+		
+		print("Finding ProductID to reference<br>");
+		
+		$stmt = $pdo->prepare("SELECT ProductId FROM Product WHERE Product_Name = :name;");
+		$stmt->bindParam(':name', $name, PDO::PARAM_STR, 32);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		$pid = $row['ProductId'];
+		
+		print("ProductID $pid found.<br>");
+		
+		$stmt = $pdo->prepare("INSERT INTO `MainDish` (`Pid`, `Size`, `IsHot`) VALUES (:pid, :size, :ishot);");
+		$stmt->bindParam(':pid', $pid, PDO::PARAM_INT);
+		$stmt->bindParam(':size', $size);
+		$stmt->bindParam(':ishot', $ishot, PDO::PARAM_INT);
+		$stmt->execute();
+		
+		$pdo->commit();
+  
+		$pdo->exec('UNLOCK TABLES');
+		print "Unlock table. Successful transaction<br>";
+	
+		$pdo = null;
+	}
+	catch(PDOException $error) {
+		$pdo->rollback();
+		print("Rollingback <br>");
+		die("ERROR: Could not complete. " . $error->getMessage());
+	}
   }
 ?>
